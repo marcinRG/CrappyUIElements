7d17b0b7b332e885ab2ed2e78e6853d2aa372305
marcinRG <marcin.guszpit@gmail.com>
2018-12-07 02:24:02 +0100

    changes

--- test/Utils/Utilities.spec.ts
+++ test/Utils/Utilities.spec.ts
@@ -2,27 +2,31 @@ import * as utils from '../../src/Utils/Utilities';
 import {PlainTextArrayWithFilterSingleSelection} from '../../src/Models/PlainTextArrayWithFilterSingleSelection';
 
 describe('Utils function tests', () => {
-    // const list11 = [
-    //     'element 1',
-    //     'element 2',
-    //     'element 3',
-    //     'element 4',
-    //     'element 5',
-    //     'element 6',
-    //     'element 7',
-    //     'element 8',
-    //     'element 9',
-    //     'element 10',
-    //     'element 11',
-    // ];
-    // const init = 'element 1';
-    // const txtArray11 = new PlainTextArrayWithFilterSingleSelection(list11, init);
+    const list11 = [
+        'element 1',
+        'element 2',
+        'element 3',
+        'element 4',
+        'element 5',
+        'element 6',
+        'element 7',
+        'element 8',
+        'element 9',
+        'element 10',
+        'element 11',
+    ];
+    const init = 'element 1';
+    const txtArray11 = new PlainTextArrayWithFilterSingleSelection(list11, init);
 
-    it('should return expeted results', () => {
-        //expect(txtArray11.selected).toBe(init);
+    it('should return expected results', () => {
+        expect(txtArray11).toBeDefined();
+        expect(txtArray11.selected).toBe(init);
     });
 
-    it('should return expeted value', ()=>{
-
+    it('should return expected value', ()=>{
+        const el = 'element 4';
+        expect(utils.findInList(txtArray11,el)).toBe(3);
+        const el2= 'elenm';
+        expect(utils.findInList(txtArray11,el2)).toBe(-1);
     })
 });
